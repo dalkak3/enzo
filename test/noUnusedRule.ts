d658ea6edbf57e1bae6d1d2c433e6ecd52996411
Gnlow <gnlow@pm.me>
2025-09-19 11:42:01 +0900

    walk

---
+++ test/noUnusedRule.ts
@@ -0,0 +1,38 @@
+import { projectSchema } from "../src/type/mod.ts"
+import { cases } from "../deps/ente.ts"
+import { z } from "../deps/zod.ts"
+
+const test =
+(schema: z.ZodType) =>
+    Object.entries(cases).every(([_name, project]) => {
+        const result = schema.safeParse(project, { reportInput: true })
+
+        return result.success
+    })
+
+const walk =
+(schema_: z.ZodType, depth = 0): z.ZodType[] => {
+    const type = schema_.type
+
+    if (type != "optional")
+        console.log(" ".repeat(depth*2)+type)
+
+    if (type == "object") {
+        const schema = schema_ as z.ZodObject
+
+        Object.fromEntries(Object.entries(schema.shape).map(([k, v]) =>
+            [k, walk(v, depth+1)]
+        ))
+
+        return Object.keys(schema.shape)
+            .map(k => schema.omit({ [k]: true }))
+    } else if (type == "optional" || type == "array") {
+        const schema = schema_ as z.ZodOptional | z.ZodArray
+        return walk(schema.unwrap() as z.ZodType, depth+0)
+    } else {
+        return [schema_]
+    }
+}
+
+walk(projectSchema)
+console.log(test(projectSchema))
